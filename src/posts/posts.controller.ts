78310d70ab70ccd15007e855fdbd93e3233c29c0
honestxll <honestxll@gmail.com>
2019-09-17 22:53:10 +0800

    查看请求头信息

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Req, Query } from '@nestjs/common';
+import { Controller, Get, Req, Query, Headers } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
   @Get()
-  index(@Req() request, @Query() query) {
+  index(@Req() request, @Query() query, @Headers() headers) {
     console.log(
       request.ip,
       request.hostname,
       request.method,
     );
     console.log(query);
+    console.log(headers);
     return [
       {
         title: 'hello',
