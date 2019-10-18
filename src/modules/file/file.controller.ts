6dc8509429cb720340726f673ed8aed20f190006
honest <1131328108@qq.com>
2019-10-18 17:42:17 +0800

    传输文件给客户端

--- src/modules/file/file.controller.ts
+++ src/modules/file/file.controller.ts
@@ -1,16 +1,32 @@
-import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, Param, Get, ParseIntPipe, Res } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileService } from './file.service';
+import { Response } from 'express';
 
 @Controller('files')
 export class FileController {
   constructor(
-    private fileService: FileService
+    private fileService: FileService,
   ) { }
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
   async store(@UploadedFile() data) {
     return await this.fileService.store(data);
+  }
+
+  @Get('serve/:id')
+  async show(
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response,
+  ) {
+    const file = await this.fileService.show(id);
+
+    res.sendFile(file.filename, {
+      root: 'uploads',
+      headers: {
+        'Content-Type': file.mimetype,
+      },
+    });
   }
 }
