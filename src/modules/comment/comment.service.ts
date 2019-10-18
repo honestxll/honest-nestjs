20b8cd7ecd09bac785e6ddc8fdbf194db0f4b1b6
honest <1131328108@qq.com>
2019-10-18 13:31:52 +0800

    存储评论内容

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comment } from './comment.entity';
+import { Repository } from 'typeorm';
+import { User } from '../user/user.entity';
+import { CommentDto } from './comment.dto';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private comentRepository: Repository<Comment>,
+  ) { }
+
+  async storePostComment(id: number, user: User, data: CommentDto) {
+    return await this.comentRepository.save({
+      user,
+      ...data,
+      post: { id },
+    });
+  }
+}
